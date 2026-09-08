f6a4d0064ebdb55bdbdc445c270551d6a1395c3b
Sampreeti Fondekar <sampreeti1010@gmail.com>
2026-09-08 15:43:00 +0530

    Created Subjects and Add Subjects Activity

--- app/src/main/java/com/example/studentplanner2026/AddSubjectActivity.kt
+++ app/src/main/java/com/example/studentplanner2026/AddSubjectActivity.kt
@@ -1,28 +1,20 @@
 package com.example.studentplanner2026
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    lateinit var subjectsButton: Button
+class AddSubjectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_add_subject)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        subjectsButton=findViewById<Button>(R.id.btnSubjects)
-        subjectsButton.setOnClickListener {
-            val intent= Intent(this, SubjectsActivity::class.java)
-            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/studentplanner2026/SubjectsActivity.kt
+++ app/src/main/java/com/example/studentplanner2026/SubjectsActivity.kt
@@ -2,26 +2,26 @@ package com.example.studentplanner2026
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
-class MainActivity : AppCompatActivity() {
-    lateinit var subjectsButton: Button
+class SubjectsActivity : AppCompatActivity() {
+    lateinit var btnAddSub : MaterialButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_subjects)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        subjectsButton=findViewById<Button>(R.id.btnSubjects)
-        subjectsButton.setOnClickListener {
-            val intent= Intent(this, SubjectsActivity::class.java)
+        btnAddSub=findViewById<MaterialButton>(R.id.btnAddSubject)
+        btnAddSub.setOnClickListener {
+            val intent= Intent(this, AddSubjectActivity::class.java)
             startActivity(intent)
         }
     }
